e8d71964109bf548b4504b22880970192320da02
Busarat <busaratjum@hotmail.com>
2012-10-03 18:21:12 +0700

    add javadoc

--- src/Voter.java
+++ src/Voter.java
@@ -1,19 +1,26 @@
 import java.util.List;
 
 /**
- * 
+ * Voter can vote in the limit quota.
+ *
  * @author Busarat Jum
  *
- * Voter Class 
  */
 public class Voter {
 	
+	/** Name of voter */
 	private String name;
+	/** The ballot that the voter get from the registration */
 	private Ballot ballot;
+	/** The teamlist object for getting teams */
 	private TeamList teamList;
+	/** The list of Team */
 	private List<Team> list;
 	
-	
+	/** 
+	 * Voter constructor.
+	 * @param name of the voter. 
+	 */
 	public Voter(String name)
 	{
 		this.name = name;
@@ -21,13 +28,21 @@ public class Voter {
 		this.list = teamList.getTeam();
 	}
 
+	/**  
+	 * Get name of the voter.
+	 * @return name is the name of voter.
+	 */
 	public String getName()
 	{
-		return this.name;
+		return name;
 	}
 	
-	
-	public List showTeam(){
+	/**  
+	 * Return a list of all team that gets from teamlist.
+	 * @return list is the list of all participate team.
+	 */
+	public List showTeam()
+	{
 		return list;
 	}
 	
