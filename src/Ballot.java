0fba0d58d4b94a110614101567cff030a60e3628
Busarat <busaratjum@hotmail.com>
2012-10-03 18:37:37 +0700

    add javadoc for ballot

--- src/Ballot.java
+++ src/Ballot.java
@@ -1,29 +1,47 @@
 /**
+ * Ballot limits quota for the voter to vote.
  * 
  * @author Busarat Jum
  *
  */
 public class Ballot {
 	
-	public int value;
+	/** The quotas for vote */
+	public int quota;
 	
+	/**  
+	 * Ballot Constructor.
+	 * @param value is the quotas for vote.
+	 */
 	public Ballot(int value)
 	{
-		this.value = value;
+		this.quota = value;
 	}
 	
+	/**  
+	 * Get the remaining quota.
+	 * @return value is the remaining quota.
+	 */
 	public int getballot()
 	{
-		return this.value;
+		return quota;
 	}
 	
+	/** 
+	 * Change the value of quota.
+	 * @param value is the amount of value for change the quota.
+	 */
 	public void setBallot(int value)
 	{
-		this.value = value;
+		this.quota = value;
 	}
+	
+	/** 
+	 * Reduce the quota by the given value.
+	 * @param value is the amount of value for change the quota.
+	 */
 	public void reduceBallot(int value)
 	{
-		
-		this.value -= value; 
+		this.quota -= value; 
 	}
 }
