2e93f916b814ab8a64089fba18439d4c49b1ccf5
Busarat <busaratjum@hotmail.com>
2012-11-20 00:46:45 +0700

    add javadoc

--- src/exceedvote/air/model/TeamList.java
+++ src/exceedvote/air/model/TeamList.java
@@ -8,62 +8,71 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 /**
- * TeamList represents the teams. TeamList contains the teams that can be voted in List. 
- * @author Prisa Dumrongsiri
+ * TeamList represents the list of all teams in the competition. TeamList
+ * contains the teams that can be voted.
+ * 
+ * @author Air Team
+ * @version 2012.11.20
  */
 @Entity
 public class TeamList implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	/** List of type Team, which keeps all the participated teams. */
 	private List<Team> listTeam;
+
 	/**
-	 * TeamList constructor initializes the list of type Team.
+	 * TeamList constructor initializes the list of Team.
 	 */
-	public TeamList()
-	{
+	public TeamList() {
 		listTeam = new ArrayList<Team>();
 	}
 
-	/** 
-	 * Get all the Teams.
-	 * @return list The List which contains all teams that exist.
+	/**
+	 * Return a list of all teams.
+	 * 
+	 * @return The List which contains all teams in the competition.
 	 */
-	public List<Team> getTeam()
-	{
+	public List<Team> getTeam() {
 		return listTeam;
 	}
 
 	/**
 	 * Add Team.
-	 * @param team is object of class Team.
+	 * 
+	 * @param team
+	 *            is object of class Team.
 	 * @return true if team is added.
 	 */
-	public boolean addTeam(Team team)
-	{
-		if(!listTeam.contains(team))
-		{
-			listTeam.add(team);
-			return true;
+	/**
+	 * Add Team to the List
+	 * 
+	 * @param team
+	 *            - Team that is in the competition.
+	 * @return true if the Team was added.
+	 */
+	public boolean addTeam(Team team) {
+		if (!listTeam.contains(team)) {
+			return listTeam.add(team);
 		}
 		return false;
 	}
 
+	/**
+	 * Return a list of team name.
+	 * 
+	 * @return List of String represents all team names.
+	 */
+	public String[] getTeamNames() {
+		String[] names = new String[listTeam.size()];
+		for (int i = 0; i < listTeam.size(); i++) {
+			names[i] = listTeam.get(i).getName();
+		}
+		return names;
+	}
 
-	
-	public String[] getTeamNames()
-    {    
-        String[] names = new String[listTeam.size()];
-        for(int i = 0 ; i < listTeam.size(); i++)
-        {
-            names[i] = listTeam.get(i).getName();
-        }
-        return names;
-    }
-
-	
 }
