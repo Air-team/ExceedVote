013ce84bfc16e30083887b9bdda493040498b4f7
Busarat <busaratjum@hotmail.com>
2012-11-20 00:43:09 +0700

    add javadoc

--- src/exceedvote/air/model/Ballot.java
+++ src/exceedvote/air/model/Ballot.java
@@ -9,140 +9,182 @@ import exceedvote.air.persistence.BallotDao;
 import exceedvote.air.persistence.DaoFactory;
 
 /**
- * Entity implementation class for Entity: Ballot
- *
+ * Ballot represent the right that each user have in their hands to vote for the
+ * team. Each ballot represent one right.
+ * 
+ * @author Air Team
+ * @version 2012.11.20
  */
 @Entity
 public class Ballot implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
-
-	public Ballot() {
-		super();
-	}
-	
-	
-   
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String teamName;
 	private String topic;
-	
-	
+
 	private Voter voter;
-	
-	
+
 	/** TeamList */
-	@OneToOne(cascade=CascadeType.PERSIST)
+	@OneToOne(cascade = CascadeType.PERSIST)
 	private TeamList teamList;
-	@OneToOne(cascade=CascadeType.PERSIST)
+	@OneToOne(cascade = CascadeType.PERSIST)
 	private Team team;
-	
+
 	/** The List of teams to vote */
 	private List<Team> list;
-	/** Log4j */
-	@OneToOne(cascade=CascadeType.PERSIST)
+	/** Log4j for tracking the action of this class */
+	@OneToOne(cascade = CascadeType.PERSIST)
 	private Tracking track = new Tracking();
-	/** Log for tracking the action of this class */
-	
 
-	
+	/**
+	 * Ballot Constructor with no parameter.
+	 */
+	private Ballot() {
+		super();
+	}
+
+	/**
+	 * Ballot Constructor that receive the list of team.
+	 * 
+	 * @param teamList is a list of all team in the competition.
+	 */
 	public Ballot(TeamList teamList) {
 		this();
-		
 		this.teamList = teamList;
 		list = teamList.getTeam();
+	}
 
-	}   
-	
-
+	/**
+	 * Return the Ballot ID. 
+	 * @return an Integer represent the ID.
+	 */
 	public Integer getId() {
 		return this.id;
 	}
 
+	/**
+	 * Return the team name. 
+	 * @return String represent team name.
+	 */
 	public String getTeamName() {
 		return teamName;
 	}
-	
+
+	/**
+	 * Set the team name. 
+	 * @param teamName is a String represent team name.
+	 */
 	public void setTeamName(String teamName) {
 		this.teamName = teamName;
 	}
-	
+
+	/**
+	 * Return the list of all team name. 
+	 * @return Array of String represent all the team names.
+	 */
+	public String[] getTeamNames() {
+		return teamList.getTeamNames();
+	}
+
+	/**
+	 * Return the the Vote topic. 
+	 * @return String represent the vote topic.
+	 */
 	public String getTopic() {
 		return topic;
 	}
-	
+
+	/**
+	 * Set the vote topic. 
+	 * @param topic is the topic for vote.
+	 */
 	public void setTopic(String topic) {
 		this.topic = topic;
 	}
-	
+
+	/**
+	 * Set the Ballot ID. 
+	 * @param id is an Integer represent the ID.
+	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
+	/**
+	 * Set the voter that has this ballot.
+	 * @param voter - Voter object that has name and type.
+	 */
 	public void setVoter(Voter voter) {
 		this.voter = voter;
 	}
-	
+
+	/**
+	 * Return the voter the has this ballot.
+	 * @return Voter object that has name and type.
+	 */
 	public Voter getVoter() {
 		return voter;
 	}
-	
+
 	/**
-	 * Put the Ballot into the BallotBox. And set the vote, that the user vote, to the team.
-	 * @param teamName is name of the team that the user wants to vote.
+	 * Put the Ballot into the BallotBox and set the score to the team. 
+	 * @param teamName - is name of the team that the user wants to vote.
+	 * @param typeTeam - the team type.
+	 * @param voter - the person who vote.
+	 * @return true if successfully put the ballot.
 	 */
-	public boolean putBallot(String teamName,String typeTeam,Voter voter) {
-		
+	public boolean putBallot(String teamName, String typeTeam, Voter voter) {
 		for (int i = 0; i < list.size(); i++) {
-			
-			if ( list.get(i).getName().equals(teamName) ) {
+			if (list.get(i).getName().equals(teamName)) {
 				Ballot ballot = new Ballot();
 				ballot.setTeamName(teamName);
 				ballot.setTopic(typeTeam);
 				ballot.setVoter(voter);
 				this.voter = voter;
-				int value  = voter.getballotLeft();
+				int value = voter.getballotLeft();
 				this.team = list.get(i);
 				int score = team.getScore(typeTeam);
 				score++;
 				team.setScore(score, typeTeam);
 				BallotDao dao = DaoFactory.getInstance().getBallotDao();
 				dao.save(ballot);
-				voter.setballotLeft(value-1);
-				track.addLogVote(teamName,typeTeam);
+				voter.setballotLeft(value - 1);
+				track.addLogVote(teamName, typeTeam);
 				return true;
 			}
-		}		
+		}
 		return false;
 	}
-	
-	public String[] getTeamNames()
-    {
-        return teamList.getTeamNames();
-    }
 
-
+	/**
+	 * To give the ballot back for the revoting.
+	 * 
+	 * @param teamName - is name of the team that the user voted.
+	 * @param typeTeam - the team type.
+	 * @param voter - the person who vote.
+	 * @return true if the user can get the ballot back.
+	 */
 	public boolean returnBallot(String teamName, String typeTeam, Voter voter) {
 		BallotDao dao = DaoFactory.getInstance().getBallotDao();
 		List<Ballot> allBallot = dao.findAll();
 		Ballot bb = dao.findSingle(teamName, typeTeam, voter, allBallot);
-		
-//		if(bb==null) System.out.println("aaa");
-		if(bb!=null){
-				this.voter = voter;
-				int value  = voter.getballotLeft();
-				int score = team.getScore(typeTeam);
-				score--;
-				team.setScore(score, typeTeam);
-				voter.setballotLeft(value+1);
-				dao.deleteBallot(bb);
-				track.addLogRevote(teamName,typeTeam);
-				return true;
-		}else
-		return false;
-		
+
+		// if(bb==null) System.out.println("aaa");
+		if (bb != null) {
+			this.voter = voter;
+			int value = voter.getballotLeft();
+			int score = team.getScore(typeTeam);
+			score--;
+			team.setScore(score, typeTeam);
+			voter.setballotLeft(value + 1);
+			dao.deleteBallot(bb);
+			track.addLogRevote(teamName, typeTeam);
+			return true;
+		} else
+			return false;
 	}
-   
+
 }
