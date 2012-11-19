57c4835035288c95fcf9feff2224aa989e17cd2c
Busarat <busaratjum@hotmail.com>
2012-11-20 00:48:44 +0700

    add javadoc

--- src/exceedvote/air/model/VoteTopic.java
+++ src/exceedvote/air/model/VoteTopic.java
@@ -4,33 +4,53 @@ import java.io.Serializable;
 import javax.persistence.*;
 
 /**
- * Entity implementation class for Entity: l
- *
+ * VoteTopic represents the topics to vote for the team.
+ * 
+ * @author Air Team.
+ * @version 2012.11.20
  */
 @Entity
 public class VoteTopic implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
+	@Id
+	private String title;
 
-	public VoteTopic() {
+	/**
+	 * VoteTopic Constructor.
+	 */
+	private VoteTopic() {
 		super();
 	}
 
-	@Id
-	private String title;
-	
-	
-	public VoteTopic(String name) {
+	/**
+	 * Initialize the VoteTopic with title.
+	 * 
+	 * @param title
+	 *            - the topic title.
+	 */
+	public VoteTopic(String title) {
 		this();
-		this.title = name;
+		this.title = title;
 	}
 
+	/**
+	 * Return the title of the topic.
+	 * 
+	 * @return String represents the title of the topic.
+	 */
 	public String getTitle() {
 		return title;
 	}
+
+	/**
+	 * Set the title of the topic.
+	 * 
+	 * @param title
+	 *            - the changed title.
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
-   
+
 }
