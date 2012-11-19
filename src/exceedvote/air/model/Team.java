a3b3f53675ce86728fd9af82aa6ef845089d28e9
Busarat <busaratjum@hotmail.com>
2012-11-20 00:44:50 +0700

    a

--- src/exceedvote/air/model/Team.java
+++ src/exceedvote/air/model/Team.java
@@ -1,65 +1,84 @@
 package exceedvote.air.model;
 
-import java.awt.*;
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.swing.*;
 
 import exceedvote.air.persistence.DaoFactory;
-import exceedvote.air.persistence.TeamDao;
 import exceedvote.air.persistence.VoteTopicDao;
 
 /**
- * Class Team represent the  
+ * Team represents the competitor in the Exceed camp.
+ * 
  * @author Air Team
+ * @version 2012.11.20
  */
 @Entity
-public class Team implements Serializable{
+public class Team implements Serializable {
 	// instance variables - replace the example below with your own
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String name;
 	private int score;
 	private VoteTopic topic;
 
 	/**
-	 * Constructor for objects of class Team
-	 * @param type 
+	 * Team constructor.
+	 */
+	private Team() {
+		super();
+	}
+
+	/**
+	 * Initialize Team that have name and description.
+	 * 
+	 * @param name
+	 *            - Team name.
+	 * @param td
+	 *            - Team description.
 	 */
 	public Team(String name, TeamDescription td) {
 		this();
-		this.name = name;		
-	}
-	
-	public Team(){
-		super();
-	}
-	public int getScore(String topicName) {
-		return score;
+		this.name = name;
 	}
 
+	/**
+	 * Return the score of this team.
+	 * 
+	 * @param topicName
+	 *            - name of the vote topic.
+	 * @return the scores of this team.
+	 */
+	public int getScore(String topicName) {
+		return this.score;
+	}
+
+	/**
+	 * Return the name of this team.
+	 * 
+	 * @return String represents name of this team.
+	 */
 	public String getName() {
 		return this.name;
 	}
 
-	public void setScore(int score,String topicName) {
+	/**
+	 * Set the score to this team by topic.
+	 * 
+	 * @param score
+	 *            - score received.
+	 * @param topicName
+	 *            - topic that the user vote to this team.
+	 */
+	public void setScore(int score, String topicName) {
 		VoteTopicDao dao2 = DaoFactory.getInstance().getVoteTopicDao();
 		topic = dao2.find(topicName);
 		this.score = score;
 	}
-	
 
 }
