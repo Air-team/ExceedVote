ebab0d533acb2cb08a74e87fb899864e3c417af4
Busarat <busaratjum@hotmail.com>
2012-11-20 00:45:37 +0700

    add javadoc

--- src/exceedvote/air/model/TeamDescription.java
+++ src/exceedvote/air/model/TeamDescription.java
@@ -1,29 +1,40 @@
 package exceedvote.air.model;
 
 /**
+ * TeamDescription represents the description of a team.
+ * 
  * @author Air Team
+ * @version 2012.11.20
  */
 public class TeamDescription {
+
+	/** the information of a team. */
 	private String info;
 
 	/**
-	 * Constructor for objects of class TeamDescription
+	 * Initialize TeamDescription with information.
+	 * 
+	 * @param info
+	 *            - the information of a team.
 	 */
 	public TeamDescription(String info) {
 		this.info = info;
 	}
 
 	/**
-	 * Set the information of each team.
-	 * @param info String represents the information of team.
+	 * Set the information of a team.
+	 * 
+	 * @param info
+	 *            - String represents the information of a team.
 	 */
 	public void setInfo(String info) {
 		this.info = info;
 	}
 
 	/**
-	 * Get the information of each team.
-	 * @return info String represents the information of team.
+	 * Get the information of a team.
+	 * 
+	 * @return String represents the information of team.
 	 */
 	public String getInfo() {
 		return this.info;
