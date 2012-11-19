dfa2280824f93cafde4c20a9e693642f9c2b7668
Busarat <busaratjum@hotmail.com>
2012-11-20 00:47:21 +0700

    add javadoc

--- src/exceedvote/air/model/Tracking.java
+++ src/exceedvote/air/model/Tracking.java
@@ -2,44 +2,57 @@ package exceedvote.air.model;
 
 import java.io.Serializable;
 
-import org.apache.log4j.Logger;import javax.persistence.Entity;
+import org.apache.log4j.Logger;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 /**
  * Class Tracking is used for track the action of each class.
- * @author Chupan
+ * 
+ * @author Air Team
+ * @version 2012.11.20
  */
 
 @Entity
-public class Tracking  implements Serializable{
-	
+public class Tracking implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	/** final attribute of Logger */
-	final static Logger lg = Logger.getLogger( Ballot.class );
+	final static Logger lg = Logger.getLogger(Ballot.class);
 
 	/**
 	 * Constructor for objects of class Tracking.
 	 */
-	public Tracking(){
+	public Tracking() {
 		lg.trace("Initialize");
 	}
 
 	/**
-	 * Add Log.
+	 * Add Log for Vote.
+	 * 
 	 * @param teamName
+	 *            - the team's name that has an action.
+	 * @param type
+	 *            - The VoteTopic
 	 */
-	public void addLogVote(String teamName,String type){
-		lg.info("vote For "+teamName+" topic: "+ type);
+	public void addLogVote(String teamName, String type) {
+		lg.info("vote For " + teamName + " topic: " + type);
 	}
-	
-	
-	public void addLogRevote(String teamName,String type){
-		lg.info("revote For "+teamName+" topic: "+ type);
+
+	/**
+	 * Add Log for ReVote.
+	 * 
+	 * @param teamName
+	 *            - the team's name that has an action.
+	 * @param type
+	 *            - The VoteTopic
+	 */
+	public void addLogRevote(String teamName, String type) {
+		lg.info("revote For " + teamName + " topic: " + type);
 	}
 }
-	
