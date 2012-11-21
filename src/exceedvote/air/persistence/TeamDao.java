93977d59f1d947405167d09387f59d6086e05c17
Busarat <busaratjum@hotmail.com>
2012-11-21 22:18:18 +0700

    add javadoc

--- src/exceedvote/air/persistence/TeamDao.java
+++ src/exceedvote/air/persistence/TeamDao.java
@@ -4,10 +4,24 @@ import java.util.List;
 
 import exceedvote.air.model.Team;
 
+/**
+ * Team Data Access Object is a DAO for Team objects. This class represents 
+ * the interface of the database that related to Team objects.
+ * 
+ * @author Air Team
+ */
 public interface TeamDao {
 
+	/**
+	 * Save Team to the persistent storage.
+	 * @param Team is object represents the competitor that can be voted.
+	 */
 	public void save(Team team);
-	public abstract List<Team> findAll();
 
+	/**
+	 * Find all the Teams and return a list of all Teams.
+	 * @return a list of all Teams that saved on the persistent storage.
+	 */
+	public abstract List<Team> findAll();
 
 }
