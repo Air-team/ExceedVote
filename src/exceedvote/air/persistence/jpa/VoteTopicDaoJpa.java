68d1de4e047c153c550de4928d786c0a5b68c748
Busarat <busaratjum@hotmail.com>
2012-11-21 22:39:12 +0700

    add javadoc

--- src/exceedvote/air/persistence/jpa/VoteTopicDaoJpa.java
+++ src/exceedvote/air/persistence/jpa/VoteTopicDaoJpa.java
@@ -8,52 +8,70 @@ import exceedvote.air.model.VoteTopic;
 import exceedvote.air.persistence.VoteTopicDao;
 
 /**
- * Persistence operations for VoteTopic objects.
- * @author Negisu
- *
+ * VoteTopic Data Access Object is a DAO for VoteTopic objects. This class represents 
+ * the interface of the database that related to VoteTopic objects.
+ * 
+ * @author Air Team
  */
 public class VoteTopicDaoJpa implements VoteTopicDao {
 	private EntityManager em;
 	private static Logger logger;
-	
+
 	public VoteTopicDaoJpa(EntityManager em) {
 		super();
 		this.em = em;
 	}
-	
+
+	/**
+	 * Find all the VoteTopic and return a list of all VoteTopics.
+	 * @return a list of all VoteTopics that saved on the persistent storage.
+	 */
 	@Override
-	public List<VoteTopic> findAll(){
+	public List<VoteTopic> findAll() {
 		String queryStatement = "SELECT vt FROM VoteTopic vt";
 		return em.createQuery(queryStatement).getResultList();
 	}
-	
-	/* (non-Javadoc)
-	 * @see exceedvote.air.persistence.jpa.VoteTopicDao#save(exceedvote.air.model.VoteTopic)
+
+	/**
+	 * Save VoteTopic to the persistent storage.
+	 * @param VoteTopic is object represents the topic for voting.
 	 */
 	@Override
 	public void save(VoteTopic topic) {
 		EntityTransaction tx = em.getTransaction();
-	
+
 		try {
 			tx.begin();
 			em.persist(topic);
 			tx.commit();
 		} catch (IllegalStateException ex) {
-			getLogger().error("Error saving topic "+topic, ex);
-			if (tx.isActive()) tx.rollback();
+			getLogger().error("Error saving topic " + topic, ex);
+			if (tx.isActive())
+				tx.rollback();
 			// should rethrow exception so the application knows
 			// that save failed
 			throw ex;
 		}
 	}
+
+	/**
+	 * Find the VoteTopic in the persistent storage by name.
+	 * @param name - The String represents the name of the VoteTopic.
+	 * @return VoteTopic object that represents the topic for judging in the competition.
+	 */
 	@Override
-	public VoteTopic find(String title){
-		return  em.find(VoteTopic.class, title);
+	public VoteTopic find(String title) {
+		return em.find(VoteTopic.class, title);
 	}
+
+	/**
+	 * Get the Logger of VoteTopicDaoJpa class.
+	 * @return new Logger if it is null else return Logger.
+	 */
 	private static Logger getLogger() {
-		if (logger == null) logger = Logger.getLogger(VoteTopicDaoJpa.class);
+		if (logger == null)
+			logger = Logger.getLogger(VoteTopicDaoJpa.class);
 		return logger;
 	}
 
-	
 }
