722ab7ac5719bfd5418bcb0215ef809f1330a585
Busarat <busaratjum@hotmail.com>
2012-11-21 22:16:09 +0700

    add javadoc

--- src/exceedvote/air/persistence/BallotDao.java
+++ src/exceedvote/air/persistence/BallotDao.java
@@ -5,15 +5,41 @@ import java.util.List;
 import exceedvote.air.model.Ballot;
 import exceedvote.air.model.Voter;
 
+/**
+ * Ballot Data Access Object is a DAO for ballot objects. This class represents 
+ * the interface of the database that related to ballot objects.
+ * 
+ * @author Air Team
+ */
 public interface BallotDao {
+	
+	/**
+	 * Save ballot to the persistent storage.
+	 * @param ballot is object represents the vote.
+	 */
 	public void save(Ballot ballot);
 
+	/**
+	 * Find all the ballots and return a list of all ballots.
+	 * @return a list of all ballots that saved on the persistent storage.
+	 */
 	public abstract List<Ballot> findAll();
 
-	
-
+	/**
+	 * Find a ballot by search for teamname, topic and voter from a list of all ballots.
+	 * @param teamName - The name of the participated team.
+	 * @param topic - The vote topic that that the user vote.
+	 * @param voter - An object represents the user that relate to the ballot.
+	 * @param allBallot - A list of all ballot for searching.
+	 * @return a ballot from this user that match with the teamname and topic.
+	 */
 	public Ballot findSingle(String teamName, String topic, Voter voter,
 			List<Ballot> allBallot);
 
-	public boolean deleteBallot(Ballot ballot);
+	/**
+	 * Delete a given ballot from the persistent storage.
+	 * @param ballot - A ballot that you want to delete.
+	 * @return false if the ballot doesn't contain in the persistent storage or it's null.
+	 */
+	boolean deleteBallot(Ballot ballot);
 }
