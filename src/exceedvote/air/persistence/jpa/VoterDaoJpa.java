20e702d965c63fc1234efd7a03bd992335f44d9d
Busarat <busaratjum@hotmail.com>
2012-11-21 22:40:01 +0700

    add javadoc

--- src/exceedvote/air/persistence/jpa/VoterDaoJpa.java
+++ src/exceedvote/air/persistence/jpa/VoterDaoJpa.java
@@ -7,26 +7,30 @@ import javax.persistence.EntityTransaction;
 
 import org.apache.log4j.Logger;
 
-
 import exceedvote.air.model.Voter;
 
 import exceedvote.air.persistence.VoterDao;
 
 /**
- * Persistence operations for VoteTopic objects.
- * @author Negisu
- *
+ * Voter Data Access Object is a DAO for Voter objects. This class represents 
+ * the interface of the database that related to Voter objects.
+ * 
+ * @author Air Team
  */
 public class VoterDaoJpa implements VoterDao {
 	private EntityManager em;
 	private static Logger logger;
-	
+
 	public VoterDaoJpa(EntityManager em) {
 		super();
 		this.em = em;
-		
+
 	}
-	
+
+	/**
+	 * Save Voter to the persistent storage.
+	 * @param Voter is object represents the user that has right to vote.
+	 */
 	@Override
 	public void save(Voter voter) {
 		EntityTransaction tx = em.getTransaction();
@@ -35,27 +39,43 @@ public class VoterDaoJpa implements VoterDao {
 			em.persist(voter);
 			tx.commit();
 		} catch (IllegalStateException ex) {
-			getLogger().error("Error saving topic "+voter, ex);
-			if (tx.isActive()) tx.rollback();
+			getLogger().error("Error saving topic " + voter, ex);
+			if (tx.isActive())
+				tx.rollback();
 			// should rethrow exception so the application knows
 			// that save failed
 			throw ex;
 		}
 	}
-	
+
+	/**
+	 * Find all the Voters and return a list of all Voters.
+	 * @return a list of all Voters that saved on the persistent storage.
+	 */
 	@Override
-	public List<Voter> findAll(){
+	public List<Voter> findAll() {
 		String queryStatement = "SELECT vt FROM Voter vt";
 		return em.createQuery(queryStatement).getResultList();
 	}
-	
-	private static Logger getLogger() {
-		if (logger == null) logger = Logger.getLogger(VoterDaoJpa.class);
-		return logger;
-	}
+
+	/**
+	 * Find the Voter in the persistent storage by ID.
+	 * @param id - The integer represents the Voter. One ID related to only one Voter.
+	 * @return Voter object that represents the registered user.
+	 */
 	@Override
-	public Voter find(Integer id){
+	public Voter find(Integer id) {
 		return em.find(Voter.class, id);
+	}
+	
+	/**
+	 * Get the Logger of VoterDaoJpa class.
+	 * @return new Logger if it is null else return Logger.
+	 */
+	private static Logger getLogger() {
+		if (logger == null)
+			logger = Logger.getLogger(VoterDaoJpa.class);
+		return logger;
 	}
 
 }
