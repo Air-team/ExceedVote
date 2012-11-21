09ad28f383e4d2c4e8593e1e83e4c6cdde8fe7e9
Busarat <busaratjum@hotmail.com>
2012-11-21 22:20:19 +0700

    add javadoc

--- src/exceedvote/air/persistence/VoteTopicDao.java
+++ src/exceedvote/air/persistence/VoteTopicDao.java
@@ -4,12 +4,31 @@ import java.util.List;
 
 import exceedvote.air.model.VoteTopic;
 
+/**
+ * VoteTopic Data Access Object is a DAO for VoteTopic objects. This class represents 
+ * the interface of the database that related to VoteTopic objects.
+ * 
+ * @author Air Team
+ */
 public interface VoteTopicDao {
 
+	/**
+	 * Save VoteTopic to the persistent storage.
+	 * @param VoteTopic is object represents the topic for voting.
+	 */
 	public void save(VoteTopic topic);
 
+	/**
+	 * Find all the VoteTopic and return a list of all VoteTopics.
+	 * @return a list of all VoteTopics that saved on the persistent storage.
+	 */
 	public abstract List<VoteTopic> findAll();
 
+	/**
+	 * Find the VoteTopic in the persistent storage by name.
+	 * @param name - The String represents the name of the VoteTopic.
+	 * @return VoteTopic object that represents the topic for judging in the competition.
+	 */
 	public abstract VoteTopic find(String name);
 
 }
