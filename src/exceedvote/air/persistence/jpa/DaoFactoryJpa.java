824c32e5cf0c41d975cc553c6f14653b28b2f4c9
Busarat <busaratjum@hotmail.com>
2012-11-21 22:27:27 +0700

    add javadoc

--- src/exceedvote/air/persistence/jpa/DaoFactoryJpa.java
+++ src/exceedvote/air/persistence/jpa/DaoFactoryJpa.java
@@ -1,77 +1,92 @@
 package exceedvote.air.persistence.jpa;
 
-import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.apache.log4j.Logger;
-import exceedvote.air.model.VoteTopic;
+
+import exceedvote.air.persistence.BallotDao;
+import exceedvote.air.persistence.DaoFactory;
+import exceedvote.air.persistence.TeamDao;
 import exceedvote.air.persistence.VoteTopicDao;
+import exceedvote.air.persistence.VoterDao;
 
 /**
- * VoteTopic Data Access Object is a DAO for VoteTopic objects. This class represents 
- * the interface of the database that related to VoteTopic objects.
- * 
+ * DaoFactoryJpa extends abstract DaoFactory.
+ * DaoFactoryJpa is used for getting the Dao objects to access to others Dao classes.
  * @author Air Team
  */
-public class VoteTopicDaoJpa implements VoteTopicDao {
+public class DaoFactoryJpa extends DaoFactory {
+	private static final String PERSISTENCE_UNIT = "exceedvote";
+	private static Logger logger = Logger.getLogger(DaoFactory.class);
+	private EntityManagerFactory emf;
 	private EntityManager em;
-	private static Logger logger;
-
-	public VoteTopicDaoJpa(EntityManager em) {
-		super();
-		this.em = em;
-	}
+	private VoteTopicDao voteTopicDao;
+	private VoterDao voterDao;
+	private BallotDao ballotDao;
+	private TeamDao teamDao;
 
 	/**
-	 * Find all the VoteTopic and return a list of all VoteTopics.
-	 * @return a list of all VoteTopics that saved on the persistent storage.
+	 * constructor for DaoFactoryJpa.
 	 */
-	@Override
-	public List<VoteTopic> findAll() {
-		String queryStatement = "SELECT vt FROM VoteTopic vt";
-		return em.createQuery(queryStatement).getResultList();
-	}
-
-	/**
-	 * Save VoteTopic to the persistent storage.
-	 * @param VoteTopic is object represents the topic for voting.
-	 */
-	@Override
-	public void save(VoteTopic topic) {
-		EntityTransaction tx = em.getTransaction();
-
+	public DaoFactoryJpa() {
 		try {
-			tx.begin();
-			em.persist(topic);
-			tx.commit();
-		} catch (IllegalStateException ex) {
-			getLogger().error("Error saving topic " + topic, ex);
-			if (tx.isActive())
-				tx.rollback();
-			// should rethrow exception so the application knows
-			// that save failed
-			throw ex;
+			emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+			em = emf.createEntityManager();
+		} catch (Exception e) {
+			logger.fatal("Error initializing JPA", e);
+			// e.printStackTrace();
 		}
 	}
 
 	/**
-	 * Find the VoteTopic in the persistent storage by name.
-	 * @param name - The String represents the name of the VoteTopic.
-	 * @return VoteTopic object that represents the topic for judging in the competition.
+	 * Get an instance of the DAO for VoteTopics.
+	 * @return a new VoteTopicDao if VoteTopicDao hasn't been created 
+	 * else return this VoteTopicDao object.
 	 */
 	@Override
-	public VoteTopic find(String title) {
-		return em.find(VoteTopic.class, title);
+	public VoteTopicDao getVoteTopicDao() {
+		if (voteTopicDao == null)
+			voteTopicDao = new VoteTopicDaoJpa(em);
+		return voteTopicDao;
 	}
 
 	/**
-	 * Get the Logger of VoteTopicDaoJpa class.
-	 * @return new Logger if it is null else return Logger.
+	 * Get an instance of the DAO for Voter.
+	 * @return a new VoterDao if VoterDao hasn't been created 
+	 * else return this VoterDao object.	
 	 */
-	private static Logger getLogger() {
-		if (logger == null)
-			logger = Logger.getLogger(VoteTopicDaoJpa.class);
-		return logger;
+	@Override
+	public VoterDao getVoterDao() {
+		if (em != null)
+			if (voterDao == null)
+				voterDao = new VoterDaoJpa(em);
+		return voterDao;
+	}
+
+	/**
+	 * Get an instance of the DAO for Ballot.
+	 * @return a new BallotDao if BallotDao hasn't been created 
+	 * else return this BallotDao object.	
+	 */
+	@Override
+	public BallotDao getBallotDao() {
+		if (ballotDao == null)
+			ballotDao = new BallotDaoJpa(em);
+		return ballotDao;
+	}
+
+	/**
+	 * Get an instance of the DAO for Team.
+	 * @return a new TeamDao if TeamDao hasn't been created 
+	 * else return this TeamDao object.	
+	 */
+	@Override
+	public TeamDao getTeamDao() {
+		if (teamDao == null)
+			teamDao = new TeamDaoJpa(em);
+		return teamDao;
 	}
 
 }
